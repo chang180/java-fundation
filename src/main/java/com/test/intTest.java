f59a6f282fc43945ba3777b6925e7b4855be75b8
張建文 <chang180@gmail.com>
2023-12-14 10:38:13 +0800

    Unit 17 finished.

--- src/main/java/com/test/intTest.java
+++ src/main/java/com/test/intTest.java
@@ -2,11 +2,12 @@ package com.test;
 
 public class intTest {
     public static void main(String[] args) {
-        Double d = new Double(3.14);
-        double d2 = d;
-        System.out.println("我要印字串：" + d);
+        int a = Integer.parseInt("4");
+        int b = 3;
 
-        String s = String.valueOf(d);
-        System.out.println("我要印另一個字串：" + s);
+        System.out.println(a % b);
+        String str = "420";
+        str += 42;
+        System.out.println(str);
     }
 }
---
+++ src/main/java/homework/Unit17.java
@@ -0,0 +1,9 @@
+package homework;
+
+public class Unit17 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        System.out.println((a != b) && (b % a > 0) ? "yes" : "no"); // yes
+    }
+}
